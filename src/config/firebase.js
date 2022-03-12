3d58742f668a1deec8278385e14e5b0f99d8f8dc
ghost <jhonsnow751@gmail.com>
2022-03-12 11:52:58 +0100

    mjjjjjfjkkh

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,12 @@ import { getStorage } from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBNSKpZ1syb0z8g8OOoye1lps1k7AcXpk8",
-  authDomain: "scrapedsite.firebaseapp.com",
-  projectId: "scrapedsite",
-  storageBucket: "scrapedsite.appspot.com",
-  messagingSenderId: "317704250761",
-  appId: "1:317704250761:web:eff67f6e124883fb61ad58",
-  measurementId: "G-WWFDG2HKPK",
+  apiKey: "AIzaSyD6hib1BaXjvs_E6385CYNqKK2mBqCWbcY",
+  authDomain: "cointradecenter-ad3f5.firebaseapp.com",
+  projectId: "cointradecenter-ad3f5",
+  storageBucket: "cointradecenter-ad3f5.appspot.com",
+  messagingSenderId: "770323173522",
+  appId: "1:770323173522:web:6fbac7eb57d92d4f52964b",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -20,6 +19,4 @@ const storage = getStorage(app);
 
 const db = getFirestore(app);
 
-
 export { storage, db };
-
